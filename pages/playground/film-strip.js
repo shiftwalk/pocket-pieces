11968f54954fae6735d9dbc15eb53b4f9d9b7199
samuelgoddard <hello@samgoddard.co.uk>
2023-03-27 16:19:19 +0100

    further playground

--- pages/playground/film-strip.js
+++ pages/playground/film-strip.js
@@ -71,12 +71,12 @@ export default function FilmStrip() {
                   <div className="whitespace-nowrap py-8 lg:py-12 will-change-transform" ref={horiScroller}>
                     <div className="w-[45vw] lg:w-[35vw] h-[63vw] lg:h-[50vw] mx-[5.5vw] lg:mx-[7.5vw] overflow-hidden rounded-3xl inline-block rotate-2 blur-[2px] relative will-change-transform" ref={horiScrollerItem1}>
                       <div className="inner-shadow absolute inset-0 w-full h-full z-[1]"></div>
-                      <Image src="https://placedog.net/700/940" width={700} height={940} className="block absolute inset-0 w-full h-full object-cover object-center z-[-1]" alt="placeholder" priority />
+                      <Image src="https://placedog.net/700/940" width={700} height={940} className="block absolute inset-0 w-full h-full object-cover object-center z-[-1]" alt="placeholder" />
                     </div>
 
                     <div className="w-[45vw] lg:w-[35vw] h-[63vw] lg:h-[50vw] mx-[5.5vw] lg:mx-[7.5vw] overflow-hidden rounded-3xl inline-block relative group will-change-transform" ref={horiScrollerItem2}>
                       <div className="inner-shadow absolute inset-0 w-full h-full z-[3]"></div>
-                      <Image src="https://placedog.net/700/940" width={700} height={940}  className="block absolute inset-0 w-full h-full object-cover object-center z-[-1]" alt="placeholder" priority />
+                      <Image src="https://placedog.net/700/940" width={700} height={940}  className="block absolute inset-0 w-full h-full object-cover object-center z-[-1]" alt="placeholder" />
                       <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity ease-in-out duration-[330ms] z-[2]">
                         <div className="absolute inset-y-[6%] inset-x-[7%] border-white border flex items-center justify-center z-[2]">
                           <div className="w-[20%] lg:w-[10%] text-white">
@@ -92,7 +92,7 @@ export default function FilmStrip() {
 
                     <div className="w-[45vw] lg:w-[35vw] h-[63vw] lg:h-[50vw] mx-[5.5vw] lg:mx-[7.5vw] overflow-hidden rounded-3xl inline-block rotate-1 blur-[2px] relative will-change-transform" ref={horiScrollerItem3}>
                       <div className="inner-shadow absolute inset-0 w-full h-full z-[1]"></div>
-                        <Image src="https://placedog.net/700/940" width={700} height={940} className="block absolute inset-0 w-full h-full object-cover object-center z-[-1]" alt="placeholder" priority />
+                        <Image src="https://placedog.net/700/940" width={700} height={940} className="block absolute inset-0 w-full h-full object-cover object-center z-[-1]" alt="placeholder" />
                     </div>
                   </div>
                 </div>
