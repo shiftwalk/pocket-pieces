a29cba70dd6b668232315cff83ac84f152b86292
samuelgoddard <hello@samgoddard.co.uk>
2023-03-29 13:47:01 +0100

    stats added

--- components/polaroid.js
+++ components/polaroid.js
@@ -1,42 +1,74 @@
 import CrosshairIcon from '@/icons/crosshair.svg'
 import Image from 'next/image';
-import { useState } from 'react';
+import { m } from 'framer-motion';
 import StarIcon from '@/icons/star.svg';
 
 export default function Polaroid({ className, image, imageWidth, imageHeight, hoverImage, hoverImageWidth, hoverImageHeight, metaHeading, metaText, noShadow, product, price }) {
-  const [shouldTransition, setShouldTransition] = useState(false);
-  function handleHover() {
-    setShouldTransition(true);
+
+  const imageVariants = {
+    initial: { scale: 1, filter: "blur(10px)" },
+    hover: {
+      scale: [1, 1.03, 1.08],
+      filter: "blur(10px)",
+      transition: { duration: 0.45, ease: [0.83, 0, 0.17, 1] }
+    }
   }
 
-  function handleHoverOut() {
-    setShouldTransition(false);
+  const backImageVariants = {
+    initial: { scale: 1, filter: "blur(10px)", opacity: 0 },
+    hover: {
+      opacity: 1,
+      scale: [1.08, 1],
+      filter: "blur(0px)",
+      transition: { duration: 0.55, delay: 0.35, ease: [0.83, 0, 0.17, 1] }
+    }
+  }
+
+  const blinkVariants = {
+    initial: { opacity: 0 },
+    hover: {
+      opacity: [0, 1, 0, 1, 0, 1],
+      transition: { duration: 0.93, ease: "linear" }
+    }
+  }
+
+  const frameVariants = {
+    initial: { opacity: 0 },
+    hover: {
+      opacity: 1,
+      transition: { duration: 0.35, ease: [0.83, 0, 0.17, 1] }
+    }
   }
 
   return (
-    <div className={`bg-white w-auto p-[9%] pb-0 relative group camera-focus-hover ${className} ${!noShadow ? 'shadow-lg shadow-black/01' : ''}`} onMouseEnter={handleHover} onMouseLeave={handleHoverOut}>
+    <m.div initial="initial" whileHover="hover" className={`bg-white w-auto p-[9%] pb-0 relative group ${className} ${!noShadow ? 'shadow-lg shadow-black/01' : ''}`}>
       <div className="block relative aspect-square overflow-hidden">
-        <Image src={image ? image : 'https://placedog.net/720/720'} width={imageWidth ? imageWidth : 720} height={imageHeight ? imageHeight : 720} className={`block w-full relative z-[10] ${ shouldTransition && 'camera-focus-hover--image' }`} alt="placeholder" />
+        <m.div
+          className="w-full h-full"
+          variants={imageVariants}
+        >
+          <Image src={image ? image : 'https://placedog.net/720/720'} width={imageWidth ? imageWidth : 720} height={imageHeight ? imageHeight : 720} className={`block w-full relative z-[10]`} alt="placeholder" />
+        </m.div>
 
         {hoverImage && (
           <div className="absolute inset-0">
-            <div className="absolute inset-y-[20%] inset-x-[10%] border-white border flex items-center justify-center z-[20] opacity-0 group-hover:opacity-100 transition-opacity ease-in-out duration-[220ms]">
+            <m.div className="absolute inset-y-[20%] inset-x-[10%] border-white border flex items-center justify-center z-[20] opacity-0" variants={frameVariants}>
               <div className="w-[20%] lg:w-[10%] text-white">
                 <CrosshairIcon className={`w-full pointer-events-none`} />
               </div>
 
-              <span className="block absolute bottom-0 left-0 text-sm uppercase text-white py-2 px-3 leading-none transition-opacity ease-in-out duration-[220ms] opacity-0 group-hover:opacity-100 z-[20]">
-                <span className={`uppercase text-sm ${ shouldTransition && 'animate-blink' }`}>P_P_001</span>
-              </span>
+              <m.div className="block absolute bottom-0 left-0 text-sm uppercase text-white py-2 px-3 leading-none z-[20]" variants={blinkVariants}>
+                <span className={`uppercase text-sm`}>P_P_001</span>
+              </m.div>
 
-              <span className="block absolute bottom-0 right-0 text-sm uppercase text-white py-2 px-3 leading-none transition-opacity ease-in-out duration-[220ms] opacity-0 group-hover:opacity-100 z-[20]">
-                <StarIcon className={`w-10 ${ shouldTransition && 'animate-blink' }`} />
-              </span>
-            </div>
+              <m.div className="block absolute bottom-0 right-0 text-sm uppercase text-white py-2 px-3 leading-none z-[20]" variants={blinkVariants}>
+                <StarIcon className={`w-10`} />
+              </m.div>
+            </m.div>
 
-            <div className="absolute inset-0 object-cover object-center z-[10] opacity-0 group-hover:opacity-100 transition-opacity ease-in-out duration-[300ms] group-hover:delay-[220ms]">
-              <Image src={hoverImage ? hoverImage : 'https://placedog.net/720/720'} width={hoverImageWidth ? hoverImageWidth : 720} height={hoverImageHeight ? hoverImageHeight : 720} className={`block w-full pointer-events-none ${ shouldTransition && 'camera-focus-hover--image--back' }`} alt="placeholder" />
-            </div>
+            <m.div variants={backImageVariants} className="absolute inset-0 object-cover object-center z-[10] opacity-0">
+              <Image src={hoverImage ? hoverImage : 'https://placedog.net/720/720'} width={hoverImageWidth ? hoverImageWidth : 720} height={hoverImageHeight ? hoverImageHeight : 720} className={`block w-full pointer-events-none`} alt="placeholder" />
+            </m.div>
           </div>
         )}
       </div>
@@ -52,6 +84,6 @@ export default function Polaroid({ className, image, imageWidth, imageHeight, ho
           <span className="block uppercase text-base lg:text-lg mt-[6px] lg:mt-[10px]">${price ? price : 'Sold Out'}</span>
         )}
       </div>
-    </div>
+    </m.div>
   )
 }
