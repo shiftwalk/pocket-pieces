d0c5a6fdea47e3fd23448217c1f8cac21e57b437
samuelgoddard <hello@samgoddard.co.uk>
2023-06-02 17:21:13 +0100

    menu proper colour fade

--- pages/shop/collections/[slug].js
+++ pages/shop/collections/[slug].js
@@ -153,13 +153,18 @@ export default function CollectionSlug(initialData) {
 
               <div className={`fixed bottom-0 w-full z-[51] hidden lg:flex justify-center transition-opacity ease-in-out duration-[250ms] ${filtersHidden ? 'opacity-0' : 'opacity-100' }`}>
                 <m.div variants={fade} className="mx-auto w-auto relative inline-block pt-16 lg:pt-16 p-3 lg:p-6">
-                  <StrikeIcon className="w-full absolute bottom-[28%] left-0 right-0 z-[30]" />
                   <div className="absolute inset-0 bg-gradient-to-t from-zinc-100 via-zinc-100 to-transparent z-[20]"></div>
                   <span className="text-[12vw] lg:text-[6.25vw] 2xl:text-[100px] font-display leading-[0.65] lg:leading-[0.65] flex justify-center relative z-[40]">
                     <Link href="/shop" className={`block`}>All,&nbsp;</Link>
                     {collections.map((e, i) => {
                       return (
-                        <Link href={`/shop/collections/${e.node.handle}`} className={`block`} key={i}><span className={`${collectionData.handle == e.node.handle ? 'bg-gradient-to-t from-zinc-100 via-zinc-100 to-transparent px-[5px]' : '' }`}>{e.node.title}</span>{(i+1) !== collections.length ? ',' : ''}&nbsp;</Link>
+                        <Link href={`/shop/collections/${e.node.handle}`} className={`block relative`} key={i}>
+                          <span className={`${collectionData.handle == e.node.handle ? 'bg-gradient-to-t from-zinc-100 via-zinc-100 to-transparent' : '' }`}>{e.node.title}</span>{(i+1) !== collections.length ? ',' : ''}&nbsp;
+
+                          {collectionData.handle == e.node.handle && (
+                            <StrikeIcon className="w-full absolute bottom-[-15%] left-0 right-0 scale-y-[350%] rotate-[-1.4deg]" />
+                          )}
+                        </Link>
                       )
                     })}
                   </span>
--- pages/shop/index.js
+++ pages/shop/index.js
@@ -149,11 +149,13 @@ export default function Shop(initialData) {
 
               <div className={`fixed bottom-0 w-full z-[51] hidden lg:flex justify-center transition-opacity ease-in-out duration-[250ms] ${filtersHidden ? 'opacity-0 pointer-events-none' : 'opacity-100' }`}>
                 <m.div variants={fade} className="mx-auto w-auto relative inline-block pt-16 lg:pt-16 p-3 lg:p-6">
-                  <StrikeIcon className="w-full absolute bottom-[28%] left-0 right-0 z-[30]" />
-
                   <div className="absolute inset-0 bg-gradient-to-t from-zinc-100 via-zinc-100 to-transparent z-[20]"></div>
                   <span className="text-[12vw] lg:text-[6.25vw] 2xl:text-[100px] font-display leading-[0.65] lg:leading-[0.65] flex justify-center relative z-[50]">
-                    <Link href="/shop" className={`block bg-zinc-100 pl-1`}><span>All</span>,&nbsp;</Link>
+                    <Link href="/shop" className={`block bg-zinc-100 relative`}>
+                      <span>All</span>,&nbsp;
+
+                      <StrikeIcon className="w-full absolute bottom-[-15%] left-0 right-0 scale-y-[1200%] rotate-[-2deg]" />
+                    </Link>
                     {collections.map((e, i) => {
                       return (
                         <Link href={`/shop/collections/${e.node.handle}`} className={`block`} key={i}>{e.node.title}{(i+1) !== collections.length ? ',' : ''}&nbsp;</Link>
