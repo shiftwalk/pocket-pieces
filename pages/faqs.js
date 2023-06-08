703dee49be2eee19a10c83a4afd62c729a8bec08
samuelgoddard <hello@samgoddard.co.uk>
2023-06-08 17:15:00 +0100

    faq sticky bug

--- pages/faqs.js
+++ pages/faqs.js
@@ -31,79 +31,77 @@ export default function FAQs(initialData) {
     <Layout>
       <NextSeo title={info.title} />
 
-      <div className="overflow-hidden">
-        <LazyMotion features={domAnimation}>
-          <m.div
-            initial="initial"
-            animate="enter"
-            exit="exit"
-          >
-            <m.div variants={fade}>
-              <main className="pt-[70px] md:pt-[75px] lg:pt-[90px] lg:pb-[5vw] text-[#C1C1C1]">
-                <Container >
-                  <div className="flex flex-wrap relative">
-                    <div className="w-full lg:w-[45%]">
-                      <div className="lg:sticky lg:top-[90px] lg:min-h-[calc(100dvh-100px)] lg:flex lg:flex-wrap">
-                        <h1 className="text-[30vw] md:text-[22.5vw] lg:text-[20vw] mb-2 leading-none md:leading-none lg:leading-none block md:hidden">{info.title}</h1>
+      <LazyMotion features={domAnimation}>
+        <m.div
+          initial="initial"
+          animate="enter"
+          exit="exit"
+        >
+          <m.div variants={fade}>
+            <main className="pt-[70px] md:pt-[75px] lg:pt-[90px] lg:pb-[5vw] text-[#C1C1C1]">
+              <Container >
+                <div className="flex flex-wrap relative">
+                  <div className="w-full lg:w-[45%]">
+                    <div className="lg:sticky lg:top-[90px] lg:min-h-[calc(100dvh-100px)] lg:flex lg:flex-wrap">
+                      <h1 className="text-[30vw] md:text-[22.5vw] lg:text-[20vw] mb-2 leading-none md:leading-none lg:leading-none block md:hidden">{info.title}</h1>
 
-                        <div className="flex flex-wrap w-full">
-                          <div className="w-full md:w-[70%] lg:w-[63%] pb-4 md:pb-0 md:pr-3 md:max-w-[45vh]">
-                            <SanityImage image={info.heroImage} className="block w-full" alt="placeholder" />
-                          </div>
-
-                          <div className="w-full md:flex-1 flex flex-wrap">
-                            <ul className="uppercase text-sm w-full">
-                              {info.sections.map((e, i) => {
-                                return (
-                                  <li key={i} className="block">
-                                    <a
-                                      href={`#${slugify(e.heading, { lower: true })}`}
-                                      className={`block transition-opacity ease-in-out duration-300 ${currentItem == slugify(e.heading, { lower: true }) ? 'opacity-100' : 'opacity-30 hover:opacity-70' }`}>
-                                        {e.heading}
-                                      </a>
-                                  </li>
-                                )
-                              })}
-                            </ul>
-
-                            <h1 className="text-[25vw] md:text-[22.5vw] lg:text-[20vw] leading-[0.65] md:leading-[0.65] lg:leading-[0.65] hidden md:block lg:hidden mt-auto w-full">{info.title}</h1>
-                          </div>
+                      <div className="flex flex-wrap w-full">
+                        <div className="w-full md:w-[70%] lg:w-[63%] pb-4 md:pb-0 md:pr-3 md:max-w-[45vh]">
+                          <SanityImage image={info.heroImage} className="block w-full" alt="placeholder" />
                         </div>
 
-                        <h1 className="text-[25vw] md:text-[22.5vw] lg:text-[20vw] mb-4 leading-[0.6] md:leading-[0.6] lg:leading-[0.6] hidden lg:block w-full mt-auto">{info.title}</h1>
-                      </div>
-                    </div>
+                        <div className="w-full md:flex-1 flex flex-wrap">
+                          <ul className="uppercase text-sm w-full">
+                            {info.sections.map((e, i) => {
+                              return (
+                                <li key={i} className="block">
+                                  <a
+                                    href={`#${slugify(e.heading, { lower: true })}`}
+                                    className={`block transition-opacity ease-in-out duration-300 ${currentItem == slugify(e.heading, { lower: true }) ? 'opacity-100' : 'opacity-30 hover:opacity-70' }`}>
+                                      {e.heading}
+                                    </a>
+                                </li>
+                              )
+                            })}
+                          </ul>
 
-                    <div className="w-full lg:w-[53.5%] pt-[16vw] md:pt-[8vw] lg:pt-[25vw] lg:ml-auto lg:pb-[20vw]">
-                      <div className="content mb-4 lg:pr-[5%]">
-                        {info.sections.map((e, i) => {
-                          return (
-                            <InView
-                              className="scroll-mt-28"
-                              as="div"
-                              rootMargin="60px 0px 60px 0px"
-                              threshold={1}
-                              id={slugify(e.heading, { lower: true })}
-                              onChange={(inView, entry) => setCurrentItem(slugify(e.heading, { lower: true }))}
-                              key={i}
-                            >
-                              <AccordionItem item={e} />
-                            </InView>
-                          )
-                        })}
+                          <h1 className="text-[25vw] md:text-[22.5vw] lg:text-[20vw] leading-[0.65] md:leading-[0.65] lg:leading-[0.65] hidden md:block lg:hidden mt-auto w-full">{info.title}</h1>
+                        </div>
                       </div>
+
+                      <h1 className="text-[25vw] md:text-[22.5vw] lg:text-[20vw] mb-4 leading-[0.6] md:leading-[0.6] lg:leading-[0.6] hidden lg:block w-full mt-auto">{info.title}</h1>
                     </div>
                   </div>
-                </Container>
-              </main>
-              
-              <div className="text-[#C1C1C1]">
-                <Footer />
-              </div>
-            </m.div>
+
+                  <div className="w-full lg:w-[53.5%] pt-[16vw] md:pt-[8vw] lg:pt-[25vw] lg:ml-auto lg:pb-[20vw]">
+                    <div className="content mb-4 lg:pr-[5%]">
+                      {info.sections.map((e, i) => {
+                        return (
+                          <InView
+                            className="scroll-mt-28"
+                            as="div"
+                            rootMargin="60px 0px 60px 0px"
+                            threshold={1}
+                            id={slugify(e.heading, { lower: true })}
+                            onChange={(inView, entry) => setCurrentItem(slugify(e.heading, { lower: true }))}
+                            key={i}
+                          >
+                            <AccordionItem item={e} />
+                          </InView>
+                        )
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </Container>
+            </main>
+            
+            <div className="text-[#C1C1C1]">
+              <Footer />
+            </div>
           </m.div>
-        </LazyMotion>
-      </div>
+        </m.div>
+      </LazyMotion>
     </Layout>
   )
 }
