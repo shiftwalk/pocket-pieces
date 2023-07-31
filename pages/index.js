324ccf4b02dc7a886f3e114156ce5da733c897ed
samuelgoddard <hello@samgoddard.co.uk>
2023-07-31 10:54:31 +0100

    landing page test

--- pages/index.js
+++ pages/index.js
@@ -4,29 +4,14 @@ import { LazyMotion, domAnimation, m } from 'framer-motion'
 import { NextSeo } from 'next-seo'
 import LogoIcon from '@/icons/logo.svg'
 import LogoMarkOutlinedIcon from "@/icons/logomark-outlined.svg";
-import SanityPageService from '@/services/sanityPageService'
-import SanityImage from '@/components/sanity-image'
-import { homeQuery } from '@/helpers/queries'
 import Button from '@/components/button'
-import { IntroContext } from '@/context/intro'
 import { MouseParallax } from 'react-just-parallax'
-import { useContext, useEffect } from 'react'
+import Image from 'next/image'
 
-const pageService = new SanityPageService(homeQuery)
-
-export default function Home(initialData) {
-  const { data: { home }  } = pageService.getPreviewHook(initialData)()
-  const [introContext, setIntroContext] = useContext(IntroContext);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIntroContext(true)
-    }, 4500);
-  },[]);
-
+export default function Home() {
   return (
     <Layout>
-      <NextSeo title={home.title} />
+      <NextSeo title="Pocket Pieces" />
       
       <div className="overflow-hidden">
         <LazyMotion features={domAnimation}>
@@ -36,46 +21,47 @@ export default function Home(initialData) {
             exit="exit"
           >
             <main className="w-full flex flex-wrap h-screen">
-              <m.article variants={fade} className="w-full">
-                <m.div variants={fade} className="absolute inset-0 h-screen">
-                  
-                  <MouseParallax isAbsolutelyPositioned enableOnTouchDevice={false} lerpEase={0.15} strength={0.0044} zIndex={0}>
-                    <div href="/shop" className="block absolute inset-0 z-0">
-                      <SanityImage image={home.image} layout="fill" className="w-full h-full scale-[1.01]" alt="Placeholder" />
-                    </div>
-                  </MouseParallax>
-
-                  <div className="h-full w-full lg:w-8/12 max-w-[65vh] 2xl:max-w-[720px] pt-[130px] pb-[50px] relative z-20 mx-auto px-4 lg:px-6">
-                    <div className="w-full bg-white mx-auto p-4 lg:p-6 h-full flex flex-wrap">
-                      <div className="w-full">
-                        <div className="w-[50px] lg:w-[50px] mx-auto mb-5">
-                          <LogoMarkOutlinedIcon />
-                        </div>
-                        
-                        {home.introText && (
-                          <p className="block uppercase text-[11px] lg:text-[12px] leading-[1.25] text-center w-11/12 xl:w-9/12 mx-auto mb-8">{home.introText}</p>
-                        )}
-
-                        {home.posterVideoReel?.asset && (
-                          <video loop={true} autoPlay="autoplay" playsInline={true} muted className={`w-11/12 lg:w-9/12 h-auto max-h-[33dvh] mx-auto`}>
-                            <source src={home.posterVideoReel.asset.url} type="video/mp4" />
-
-                            Sorry. Your browser does not support the video tag.
-                          </video>
-                        )}
-
-                        <div className="flex flex-wrap items-center justify-center space-x-4 pb-3 -mt-12 relative z-10">
-                          <Button outline href="/shop" label="Let's Shop!" />
-                        </div>
+              <m.article variants={fade} className="w-full h-full">
+                <m.div variants={fade} className="h-full w-full flex flex-col lg:flex-wrap">  
+                  <div className="w-full lg:w-[45%] relative overflow-hidden h-[65vh] lg:h-full">
+                    <MouseParallax isAbsolutelyPositioned enableOnTouchDevice={false} lerpEase={0.25} strength={0.0144} zIndex={0}>
+                      <div className="block absolute inset-0 z-0 ">
+                        <Image src="/images/landing.jpg" fill className="w-full h-full scale-[1.03] object-center object-cover" alt="Placeholder" />
                       </div>
-                      
-                      <div className="w-full mt-auto">
-                        <LogoIcon className={`w-9/12 2xl:w-11/12 mx-auto text-black mb-5`} />
-                        {home.footerText && (
-                          <p className="block uppercase text-[11px] lg:text-[12px] leading-[1.25] text-center w-11/12 lg:w-9/12 mx-auto mb-0 pb-0">{home.footerText}</p>
-                        )}
-                      </div>
+                    </MouseParallax>
+                    <div className="absolute inset-0 w-full h-full bg-gradient-to-t from-black to-transparent opacity-30"></div>
+
+                    <div className="absolute bottom-0 left-0 right-0 w-full">
+                      <span className="block font-mono text-[10px] lg:text-xs uppercase text-off-white w-10/12 lg:w-8/12 mx-auto mb-2 text-center leading-[1.1] lg:leading-[1.1]">One-of-a-kind, sustainably-sourced, vintage pieces, curated by us for your scene-stealing looks. For all genders, sizes and occasions.</span>
+
+                      <LogoIcon className={`w-full p-5 text-white`} />
                     </div>
+                  </div>
+
+                  <div className="h-full lg:h-full w-full lg:w-[55%] p-4 lg:p-6 flex flex-col">
+                    <div className="w-[60px] lg:w-[65px] mx-auto mb-auto">
+                      <LogoMarkOutlinedIcon />
+                    </div>
+                    
+                    <div className="w-full mb-auto">
+                      <h1 className="block text-[16vw] lg:text-[5.8vw] leading-[0.75] text-center mb-8">Our online store hits screens very soon. Sign up to get notified of our opening drop.</h1>
+
+                      <form className="w-full lg:w-10/12 mx-auto block relative">
+                        <input type="email" placeholder="Email Address" className="border-b border-black w-full appearance-none uppercase text-black placeholder:text-black font-mono text-sm py-2 focus:outline-none focus:border-opacity-50" />
+
+                        <button type="submit" className="absolute bottom-0 right-0 w-6 py-3">
+                          <svg className="w-full" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 28 10"><path fill="#242B2D" d="M27.94 5.46c-1.894.906-3.654 2.346-5.28 4.32h-1.4c.4-.854.8-1.6 1.2-2.24.373-.64.746-1.174 1.12-1.6H.26V4.26h23.32c-.374-.454-.747-1-1.12-1.64-.4-.64-.8-1.374-1.2-2.2h1.4c1.626 1.946 3.386 3.386 5.28 4.32v.72Z"/></svg>
+                        </button>
+                      </form>
+                    </div>
+
+                    <span className="bg-black rounded-full w-full flex flex-wrap justify-center lg:justify-start text-off-white text-xs py-[13px] px-4 lg:px-5">
+                      <span className="block text-center mb-3 lg:mb-0">This site uses cookies to improve your visiting experience</span>
+                      <span className="flex space-x-3 ml-auto justify-center w-full lg:w-auto">
+                        <span className="block">Read More</span>
+                        <span className="block">Okay</span>
+                      </span>
+                    </span>
                   </div>
                 </m.div>
               </m.article>
@@ -85,11 +71,4 @@ export default function Home(initialData) {
       </div>
     </Layout>
   )
-}
-
-export async function getStaticProps(context) {
-  const props = await pageService.fetchQuery(context)
-  return { 
-    props: props
-  };
 }
--- pages/privacy.js
+++ pages/privacy.js
@@ -4,19 +4,12 @@ import Container from '@/components/container'
 import { fade } from '@/helpers/transitions'
 import { LazyMotion, domAnimation, m } from 'framer-motion'
 import { NextSeo } from 'next-seo'
-import AccordionItem from '@/components/accordion-item'
-import slugify from "slugify"
 import { useContext, useEffect, useState } from 'react'
-import SanityPageService from '@/services/sanityPageService'
-import { privacyQuery } from '@/helpers/queries'
-import SanityImage from '@/components/sanity-image'
-import { InView } from 'react-intersection-observer';
 import { IntroContext } from '@/context/intro'
 
 const pageService = new SanityPageService(privacyQuery)
 
-export default function Privacy(initialData) {
-  const { data: { privacy }  } = pageService.getPreviewHook(initialData)()
+export default function Privacy() {
   const [currentItem, setCurrentItem] = useState(false)
   const [introContext, setIntroContext] = useContext(IntroContext);
 
@@ -26,7 +19,7 @@ export default function Privacy(initialData) {
 
   return (
     <Layout>
-      <NextSeo title={privacy.title} />
+      <NextSeo title={"Privacy Policy"} />
 
       <LazyMotion features={domAnimation}>
         <m.div
@@ -40,53 +33,25 @@ export default function Privacy(initialData) {
                 <div className="flex flex-wrap relative">
                   <div className="w-full lg:w-[45%]">
                     <div className="lg:sticky lg:top-[90px] lg:min-h-[calc(100dvh-100px)] lg:flex lg:flex-wrap">
-                      <h1 className="text-[30vw] md:text-[22.5vw] lg:text-[20vw] mb-2 leading-none md:leading-none lg:leading-none block md:hidden">{privacy.title}</h1>
+                      <h1 className="text-[30vw] md:text-[22.5vw] lg:text-[20vw] mb-2 leading-none md:leading-none lg:leading-none block md:hidden">Privacy Policy</h1>
 
                       <div className="flex flex-wrap w-full">
                         <div className="w-full md:w-[70%] lg:w-[63%] pb-4 md:pb-0 md:pr-3 md:max-w-[45vh]">
-                          <SanityImage image={privacy.heroImage} className="block w-full" alt="placeholder" />
+                          {/* IMAGE */}
                         </div>
 
                         <div className="w-full md:flex-1 flex flex-wrap">
-                          <ul className="uppercase text-sm w-full">
-                            {privacy.sections.map((e, i) => {
-                              return (
-                                <li key={i} className="block">
-                                  <a
-                                    href={`#${slugify(e.heading, { lower: true })}`}
-                                    className={`block transition-opacity ease-in-out duration-300 ${currentItem == slugify(e.heading, { lower: true }) ? 'opacity-100' : 'opacity-30 hover:opacity-50' }`}>
-                                      {e.heading}
-                                    </a>
-                                </li>
-                              )
-                            })}
-                          </ul>
-
-                          <h1 className="text-[25vw] md:text-[22.5vw] lg:text-[20vw] leading-[0.65] md:leading-[0.65] lg:leading-[0.65] hidden md:block lg:hidden mt-auto w-full">{privacy.title}</h1>
+                          <h1 className="text-[25vw] md:text-[22.5vw] lg:text-[20vw] leading-[0.65] md:leading-[0.65] lg:leading-[0.65] hidden md:block lg:hidden mt-auto w-full">Privacy Policy</h1>
                         </div>
                       </div>
 
-                      <h1 className="text-[25vw] md:text-[22.5vw] lg:text-[20vw] mb-4 leading-[0.6] md:leading-[0.6] lg:leading-[0.6] hidden lg:block w-full mt-auto">{privacy.title}</h1>
+                      <h1 className="text-[25vw] md:text-[22.5vw] lg:text-[20vw] mb-4 leading-[0.6] md:leading-[0.6] lg:leading-[0.6] hidden lg:block w-full mt-auto">Privacy Policy</h1>
                     </div>
                   </div>
 
                   <div className="w-full lg:w-[53.5%] pt-[16vw] md:pt-[8vw] lg:pt-[25vw] lg:ml-auto lg:pb-[20vw]">
                     <div className="content mb-4 lg:pr-[5%]">
-                      {privacy.sections.map((e, i) => {
-                        return (
-                          <InView
-                            className="scroll-mt-28"
-                            as="div"
-                            rootMargin="60px 0px 60px 0px"
-                            threshold={1}
-                            id={slugify(e.heading, { lower: true })}
-                            onChange={(inView, entry) => setCurrentItem(slugify(e.heading, { lower: true }))}
-                            key={i}
-                          >
-                            <AccordionItem item={e} />
-                          </InView>
-                        )
-                      })}
+                      Content..
                     </div>
                   </div>
                 </div>
@@ -99,11 +64,4 @@ export default function Privacy(initialData) {
       </LazyMotion>
     </Layout>
   )
-}
-
-export async function getStaticProps(context) {
-  const props = await pageService.fetchQuery(context)
-  return { 
-    props: props
-  };
 }
